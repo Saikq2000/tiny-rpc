e69ea130027f523723f6f9a125af80b80e2c3a9d
Saikq2000 <837210059@qq.com>
2025-07-17 10:22:56 +0800

    version-1

---
+++ build/example/caller/CMakeFiles/consumer.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/neardi/project/mprpc/example/friend.pb.cc" "/home/neardi/project/mprpc/build/example/caller/CMakeFiles/consumer.dir/__/friend.pb.cc.o"
+  "/home/neardi/project/mprpc/example/caller/callfriendservice.cc" "/home/neardi/project/mprpc/build/example/caller/CMakeFiles/consumer.dir/callfriendservice.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../src/tiny_muduo/base"
+  "../src/tiny_muduo/logger"
+  "../src/tiny_muduo/http"
+  "../src/tiny_muduo/memory"
+  "../src/tiny_muduo/mysql"
+  "../src/tiny_muduo/net"
+  "../src/tiny_muduo/net/poller"
+  "../src/tiny_muduo/timer"
+  "../example"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/mprpc.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/neardi/project/mprpc/src/logger.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/logger.cc.o"
+  "/home/neardi/project/mprpc/src/mprpcapplication.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o"
+  "/home/neardi/project/mprpc/src/mprpcchannel.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcchannel.cc.o"
+  "/home/neardi/project/mprpc/src/mprpcconfig.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcconfig.cc.o"
+  "/home/neardi/project/mprpc/src/mprpccontroller.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/mprpccontroller.cc.o"
+  "/home/neardi/project/mprpc/src/rpcheader.pb.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/rpcheader.pb.cc.o"
+  "/home/neardi/project/mprpc/src/rpcprovider.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/rpcprovider.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/base/CurrentThread.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/base/CurrentThread.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/base/Thread.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/base/Thread.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/base/ThreadPool.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/base/ThreadPool.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/base/Timestamp.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/base/Timestamp.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/http/HttpContext.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/http/HttpContext.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/http/HttpResponse.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/http/HttpResponse.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/http/HttpServer.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/http/HttpServer.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/logger/AsyncLogging.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/logger/AsyncLogging.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/logger/FileUtil.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/logger/FileUtil.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/logger/LogFile.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/logger/LogFile.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/logger/LogStream.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/logger/LogStream.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/logger/Logging.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/logger/Logging.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/memory/MemoryPool.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/memory/MemoryPool.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/mysql/ConnectionPool.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/mysql/ConnectionPool.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/mysql/MysqlConn.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/mysql/MysqlConn.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/Acceptor.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/Acceptor.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/Buffer.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/Buffer.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/Channel.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/Channel.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/EventLoop.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/EventLoop.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/EventLoopThread.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/EventLoopThread.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/EventLoopThreadPool.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/EventLoopThreadPool.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/InetAddress.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/InetAddress.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/Socket.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/Socket.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/TcpConnection.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/TcpConnection.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/TcpServer.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/TcpServer.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/poller/DefaultPoller.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/poller/DefaultPoller.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/poller/EPollPoller.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/poller/EPollPoller.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/net/poller/Poller.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/net/poller/Poller.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/timer/Timer.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/timer/Timer.cc.o"
+  "/home/neardi/project/mprpc/src/tiny_muduo/timer/TimerQueue.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/tiny_muduo/timer/TimerQueue.cc.o"
+  "/home/neardi/project/mprpc/src/zookeeperutil.cc" "/home/neardi/project/mprpc/build/src/CMakeFiles/mprpc.dir/zookeeperutil.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../src/tiny_muduo/base"
+  "../src/tiny_muduo/logger"
+  "../src/tiny_muduo/http"
+  "../src/tiny_muduo/memory"
+  "../src/tiny_muduo/mysql"
+  "../src/tiny_muduo/net"
+  "../src/tiny_muduo/net/poller"
+  "../src/tiny_muduo/timer"
+  "../example"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
